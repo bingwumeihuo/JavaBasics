64b95fddcbed4e2b28271b7987baa791261f088f
bingwumeihuo <1183223389@qq.com>
2019-09-19 15:26:51 +0800

    增加耗时统计

--- src/main/java/com/yixin/array/dichotomy.java
+++ src/main/java/com/yixin/array/dichotomy.java
@@ -1,6 +1,5 @@
 package com.yixin.array;
 
-
 /**
  * @Package: com.yixin.array
  * @ClassName: demo1
@@ -8,19 +7,21 @@ package com.yixin.array;
  * @Author: 式神
  * @CreateDate: 2019/9/18 9:52
  */
-public class demo1 {
+public class dichotomy {
     public static void main(String[] args) {
         int[] arr3 = new int[]{-99,-54,-2,0,2,33,43,256,999};
         boolean isFlag = true;
-        int number=2563;
+        int number=999;
         int head = 0;
         int end = arr3.length - 1;
         boolean flag=true;
+        long now =System.currentTimeMillis();
         while (head<=end){
             int middle =(head+end)/2;
             if (number==arr3[middle]){
                 flag=false;
                 System.out.println("找到数据位置为"+middle);
+                System.out.println(System.currentTimeMillis()-now+"ms");
                 break;
             }else  if (number<arr3[middle]){
                 end =middle-1;
