1900dc3b0e837abc9574c12f7b3c032cb01deab0
bingwumeihuo <1183223389@qq.com>
2019-10-16 15:32:45 +0800

    工资计算

--- src/main/java/com/yixin/wages/Cui.java
+++ src/main/java/com/yixin/wages/Cui.java
@@ -8,6 +8,10 @@ package com.yixin.wages;
  * @CreateDate: 2019/10/16 13:44
  */
 public class Cui {
+    public static void main(String[] args) {
+        Wages wages = new Wages(19, 0, 8);
+        System.out.println("您的到手工资为:  " + wages.money() + "元");
+    }
     static class Wages {
         private static final int wages = 7000;
         private static final double social = 517.5;
@@ -15,18 +19,22 @@ public class Cui {
         private static final int fund = 329;
         private int wokingDay;
         private int leave;
+        private int hours;
         private double money;
-        public Wages(int wokingDay, int leave) {
+
+        public Wages(int wokingDay, int leave, int hours) {
             this.wokingDay = wokingDay;
             this.leave = leave;
+            this.hours = hours;
         }
         public double money() {
-            return money = wages / wokingDay * (wokingDay - leave) - social - fund - tax;
+            double dayMoney = wages / wokingDay;
+            double hoursMoney=dayMoney/8*1.5;
+            int Day = wokingDay - leave;
+            double other = social + fund + tax;
+            money = dayMoney * Day  + (hoursMoney * hours)- other;
+            return money;
         }
-    }
-    public static void main(String[] args) {
-        Wages wages = new Wages(20, 1);
-        System.out.println("您的到手工资为:  " + wages.money() + "元");
     }
 
 }
